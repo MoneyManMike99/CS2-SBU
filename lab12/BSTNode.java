2a734c745dc3509476fbebe68937d3c072543685
Michael Buxbaum <31252567+MoneyManMike99@users.noreply.github.com>
2018-05-13 21:29:57 -0500

    Add files via upload

---
+++ lab12/BSTNode.java
@@ -0,0 +1,60 @@
+package binarysearchtree;
+
+public class BSTNode
+{
+  protected String key;
+  protected Object element;
+  protected BSTNode left, right;
+  protected int height;
+  protected int newHeight; 
+  public BSTNode (String key, Object element, BSTNode left, BSTNode right)
+  {
+    this.key = key;
+    this.element = element;
+    this.left = left;
+    this.right = right;
+  } // BSTNode constructor
+
+  public String getKey ()
+  {
+    return key;
+  } // getKey method
+
+  public Object getElement ()
+  {
+    return element;
+  } // getElement method
+
+  public BSTNode getLeft ()
+  {
+    return left;
+  } // getLeft method
+
+  public BSTNode getRight ()
+  {
+    return right;
+  } // getRight method
+
+  public void setElement (Object element)
+  {
+    this.element = element;
+  } // setElement method
+
+  public void setLeft (BSTNode node)
+  {
+    left = node;
+  } // setLeft method
+
+  public void setRight (BSTNode node)
+  {
+    right = node;
+  } // setRight method
+  
+  public int getHeight(){
+    return height;
+  }
+  
+  public void setHeight(int newHeight){
+    height = newHeight;
+  }
+} /* BSTNode class */
